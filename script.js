27dc280483ba0ad2cc5aaf6d0b1648cfa3297480
Yohai Wiener <125396366+Monkhai@users.noreply.github.com>
2023-04-01 20:01:32 -0400

    yes

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 import addSkill from './addSkill.js';
-import { skillButtonEl } from './selectors.js';
-import { deleteBtnEl } from './selectors.js';
-import { resetBtnEl } from './selectors.js';
-import { skillValuesEl } from './selectors.js';
 import deleteSkill from './deleteSkill.js';
 import resetCalc from './resetCalc.js';
+import {
+  skillButtonEl,
+  deleteBtnEl,
+  resetBtnEl,
+  skillValuesEl,
+} from './selectors.js';
 // This sets the value of each skillValue element to 0. This allows the total difficulty to calculate difficulty of not finished passes.
 skillValuesEl.forEach((skillValue) => {
   skillValue.value = 0;
